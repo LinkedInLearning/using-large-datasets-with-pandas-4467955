f11661b5b23c747f8af4fca8f10c3112fa13b497
Miki Tebeka <miki@353solutions.com>
2023-12-10 12:26:10 +0200

    nicer dask

--- Ch04/04_01/dask_demo.py
+++ Ch04/04_01/dask_demo.py
@@ -4,6 +4,8 @@ import dask.dataframe as ddf
 file_name = '../../data/yellow_tripdata_2021-02.parquet'
 df = ddf.read_parquet(file_name)
 df
+
 # %%
 credit = df[df['payment_type'] == 2]
-credit['tip_amount'] / credit['total_amount']
+tip_pct = credit['tip_amount'] / credit['total_amount']
+max_tip = tip_pct.max()
