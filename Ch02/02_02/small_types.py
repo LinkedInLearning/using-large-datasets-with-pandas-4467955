3bcb54c871d6d84262c3f323765abd09168a4116
Miki Tebeka <miki@353solutions.com>
2023-12-12 14:49:02 +0000

    now arrow and reorg

--- Ch02/02_02/small_types.py
+++ Ch02/02_02/small_types.py
@@ -5,11 +5,6 @@ import pandas as pd
 file_name = '../../data/yellow_tripdata_2021-02.parquet'
 df = pd.read_parquet(file_name)
 
-# %% Measure Memory
-
-mb = 1_000_000
-df.memory_usage(deep=True).sum() / mb
-
 # %% Look at types
 df.dtypes
 
@@ -23,14 +18,11 @@ np.finfo(np.float32)
 
 # %% Smaller Size
 
-amount = df['total_amount'].astype(np.float32)
+mb = 1_000_000
 df['total_amount'].memory_usage(deep=True) / mb
 
 # %% Original Size
-amount.memory_usage(deep=True) / mb
 
+amount = df['total_amount'].astype(np.float32)
+amount.memory_usage(deep=True) / mb
 # %%
-df = pd.read_parquet(file_name, dtype_backend='pyarrow')
-df['total_amount'].memory_usage(deep=True) / mb
-# %%
-df.dtypes
