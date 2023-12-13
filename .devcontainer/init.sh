b9c4c81adc2297571a96a268eff6f252e0e21db7
Miki Tebeka <miki@353solutions.com>
2023-12-13 16:34:33 +0200

    install gcloud & graphviz

---
+++ .devcontainer/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+apt update
+# Required for dask visualzation
+apt install -y graphviz 
+
+# Python depdencies
+pip install -r requirements.txt
+
+# gcloud command
+cd ~
+curl -LO https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-457.0.0-linux-x86_64.tar.gz
+tar xzf google-cloud-cli-457.0.0-linux-x86_64.tar.gz
+rm google-cloud-cli-457.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh  --quiet
+echo 'PATH="${HOME}/google-cloud-sdk/bin:${PATH}"' >> ~/.bashrc
+
+# Set prompt
+echo 'PS1="$ "' >> ~/.bashrc
