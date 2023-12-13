4cb31248023b2c9a99c2870c57a5a4b5c3b4dd1a
Miki Tebeka <miki@353solutions.com>
2023-12-13 14:44:47 +0200

    Switching to sqlite3 (duckdb API not stable yet)

--- Ch03/03_02/sql.py
+++ Ch03/03_02/sql.py
@@ -1,14 +1,14 @@
 # %%
-import duckdb
+import sqlite3
 import pandas as pd
 
-db_file = '../../data/yellow_tripdata_2021-02.ddb'
+db_file = '../../data/yellow_tripdata_2021-02.db'
 query_sql = '''
-SELECT VendorID, SUM(total_amount)
+SELECT VendorID, SUM(total_amount) AS revenue
 FROM rides
 GROUP BY VendorID
 '''
 
-conn = duckdb.connect(db_file)
+conn = sqlite3.connect(db_file)
 df = pd.read_sql(query_sql, conn)
 df
