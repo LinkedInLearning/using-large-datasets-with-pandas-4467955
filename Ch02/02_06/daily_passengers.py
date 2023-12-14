5aeda15081a22a7335d3c0200460ddc6c3cd160e
Miki Tebeka <miki@353solutions.com>
2023-12-14 09:46:45 +0000

    space

--- Ch02/02_06/daily_passengers.py
+++ Ch02/02_06/daily_passengers.py
@@ -11,5 +11,3 @@ file_name = '../../data/yellow_tripdata_2021-02.parquet'
 columns=['tpep_pickup_datetime', 'passenger_count']
 df = pd.read_parquet(file_name, columns=columns)
 df.groupby(df['tpep_pickup_datetime'].dt.round('D'))['passenger_count'].sum()
-
-# %%
